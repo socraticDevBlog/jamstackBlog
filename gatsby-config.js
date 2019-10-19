64ac82e8ee19d513995388dfc2c52a40e93c65b1
socraticDevBlog <41236715+socraticDevBlog@users.noreply.github.com>
2019-10-19 19:26:43 -0400

    correctif et fichier config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `SocraticDev Blog`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `Un lieu pour parler des passions : la technologie, la philosophie et la culture générale`,
     author: `@gatsbyjs`,
   },
   plugins: [
