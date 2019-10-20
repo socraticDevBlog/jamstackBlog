1e95dbd6b2addd610193748682b4ccbd8e630fb1
socraticDevBlog <41236715+socraticDevBlog@users.noreply.github.com>
2019-10-20 18:02:33 -0400

    post communication

--- src/util/util-functions.js
+++ src/util/util-functions.js
@@ -1,7 +1,12 @@
 const slugify = function(text) {
-   return text
-     .toString()
-     .toLowerCase()
+  var filteredText = text.toString().toLowerCase(); 
+
+  // removes diactrics and accents from string
+  // useful for my french blog ;)
+  //
+  var accentLessText = filteredText.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
+  
+  return accentLessText
      .replace(/\s+/g, '-') 		// Replace spaces with -
      .replace(/[^\w-]+/g, '') 	// Remove all non-word chars
      .replace(/--+/g, '-') 		// Replace multiple - with single -
