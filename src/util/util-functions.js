de91f76c20e9a12f9e205389471a53ae0e692113
socraticDevBlog <41236715+socraticDevBlog@users.noreply.github.com>
2019-10-20 19:55:03 -0400

    corrections

--- src/util/util-functions.js
+++ src/util/util-functions.js
@@ -1,17 +1,17 @@
 const slugify = function(text) {
-  var filteredText = text.toString().toLowerCase(); 
+  var lowercasedstring = text.toString().toLowerCase(); 
 
   // removes diactrics and accents from string
   // useful for my french blog ;)
   //
-  var accentLessText = filteredText.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
+  var accentlessText = lowercasedstring.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
   
-  return accentLessText
-     .replace(/\s+/g, '-') 		// Replace spaces with -
+  return accentlessText
+     .replace(/\s+/g, '-') 		  // Replace spaces with -
      .replace(/[^\w-]+/g, '') 	// Remove all non-word chars
-     .replace(/--+/g, '-') 		// Replace multiple - with single -
-     .replace(/^-+/, '') 		   // Trim - from start of text
-     .replace(/-+$/, '') 		   // Trim - from end of text
+     .replace(/--+/g, '-') 		  // Replace multiple - with single -
+     .replace(/^-+/, '') 		    // Trim - from start of text
+     .replace(/-+$/, '') 		    // Trim - from end of text
  }
 
  module.exports = { slugify }
