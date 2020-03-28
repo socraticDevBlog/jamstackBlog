8306a0d22ae8611e35805a5e938fde48c0aac9b5
socraticDevBlog <maximebonin@outlook.com>
2020-03-27 21:49:27 -0400

    new favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -99,7 +99,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/favicon_socdev.png`, // This path is relative to the root of the site.
+        icon: `src/images/binary.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-transformer-remark`,
