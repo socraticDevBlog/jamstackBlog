1a2ae82865548b7a055b75bcec9ed2aebe5f38d8
socraticDevBlog <41236715+socraticDevBlog@users.noreply.github.com>
2020-04-18 20:39:08 -0400

    add sortingKeys function

--- src/util/util-functions.js
+++ src/util/util-functions.js
@@ -1,3 +1,19 @@
+const tagsSorted = function(dict) {
+    let keys = Object.keys(dict);
+
+    for (var i = keys.length; i >= 0; i--) {
+      for (var j = keys.length; j > 0; j--) {
+        if (dict[keys[i]] < dict[keys[j]]) {
+          let temp = keys[j];
+          keys[j] = keys[i];
+          keys[i] = temp;
+        }
+      }
+    }
+
+    return keys;
+}
+
 const slugify = function(text) {
   let lowercasedstring = text.toString().toLowerCase(); 
 
