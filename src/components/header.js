e9effa99e134c420d0d3a4f2582b9632d33c32de
socraticDev <maximebonin@outlook.com>
2020-07-14 21:01:39 -0400

    added svg head asset

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,6 @@
 import PropTypes from "prop-types"
 import React from "react"
+import Logo from "../images/head.svg"
 
 import {
   Collapse,
@@ -29,18 +30,33 @@ class Header extends React.Component {
     return (
       <Navbar fixed="top" light expand="sm">
         <div className="container">
-          <NavbarBrand href="/" title="Accueil">{this.props.siteTitle}</NavbarBrand>
+          <NavbarBrand href="/" title="Accueil">
+            <img
+              class="mr-2"
+              src={Logo}
+              width="30"
+              height="30"
+              alt="head logo"
+            />
+            {this.props.siteTitle}
+          </NavbarBrand>
           <NavbarToggler onClick={this.toggle} />
           <Collapse isOpen={this.state.isOpen} navbar>
             <Nav className="ml-auto" navbar>
               <NavItem>
-                <NavLink href="/about" title="À propos">À propos</NavLink>
+                <NavLink href="/about" title="À propos">
+                  À propos
+                </NavLink>
               </NavItem>
               <NavItem>
-                <NavLink href="/tags" title="Sujets abordés">Sujets abordés</NavLink>
+                <NavLink href="/tags" title="Sujets abordés">
+                  Sujets abordés
+                </NavLink>
               </NavItem>
               <NavItem>
-                <NavLink href="/avis" title="Avis">Avis</NavLink>
+                <NavLink href="/avis" title="Avis">
+                  Avis
+                </NavLink>
               </NavItem>
               <NavItem>
                 <NavLink href="/rss.xml" title="Feed RSS">
