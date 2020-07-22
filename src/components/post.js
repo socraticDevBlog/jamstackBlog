c5ad22f30b42f767414eacf675ad6bd941dd1ccb
socraticDev <maximebonin@outlook.com>
2020-07-22 19:00:41 -0400

    fixed url

--- src/components/post.js
+++ src/components/post.js
@@ -14,12 +14,12 @@ import { slugify } from "../util/util-functions"
 const Post = ({ title, author, slug, date, body, fluid, tags }) => {
   return (
     <Card>
-      <Link to={slug}>
+      <Link to={`/${slug}`}>
         <Img className="card-image-top" fluid={fluid} />
       </Link>
       <CardBody>
         <CardTitle>
-          <Link to={slug}>{title}</Link>
+          <Link to={`/${slug}`}>{title}</Link>
         </CardTitle>
         <CardSubtitle>
           <span className="text-info">{date}</span> par{" "}
@@ -35,7 +35,7 @@ const Post = ({ title, author, slug, date, body, fluid, tags }) => {
             </li>
           ))}
         </ul>
-        <Link to={slug} className="btn btn-outline-primary float-right">
+        <Link to={`/${slug}`} className="btn btn-outline-primary float-right">
           Lire davantage
         </Link>
       </CardBody>
