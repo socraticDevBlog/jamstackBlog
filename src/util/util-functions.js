a29d94f915d1b0390e8fab22fa677dc7a138998d
Maxime Bonin <maximebonin@outlook.com>
2023-11-20 19:27:15 -0500

    escape colon char from titles

--- src/util/util-functions.js
+++ src/util/util-functions.js
@@ -1,33 +1,36 @@
-const tagsSorted = function(dict) {
-    let keys = Object.keys(dict);
+const tagsSorted = function (dict) {
+  let keys = Object.keys(dict)
 
-    for (var i = keys.length; i >= 0; i--) {
-      for (var j = keys.length; j > 0; j--) {
-        if (dict[keys[i]] < dict[keys[j]]) {
-          let temp = keys[j];
-          keys[j] = keys[i];
-          keys[i] = temp;
-        }
+  for (var i = keys.length; i >= 0; i--) {
+    for (var j = keys.length; j > 0; j--) {
+      if (dict[keys[i]] < dict[keys[j]]) {
+        let temp = keys[j]
+        keys[j] = keys[i]
+        keys[i] = temp
       }
     }
+  }
 
-    return keys;
+  return keys
 }
 
-const slugify = function(text) {
-  let lowercasedstring = text.toString().toLowerCase(); 
+const slugify = function (text) {
+  let lowercasedstring = text.toString().toLowerCase()
 
   // removes diactrics and accents from string
   // useful for my french blog ;)
   //
-  let accentlessText = lowercasedstring.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
-  
-  return accentlessText
-     .replace(/\s+/g, '-') 		  // Replace spaces with -
-     .replace(/[^\w-]+/g, '') 	// Remove all non-word chars
-     .replace(/--+/g, '-') 		  // Replace multiple - with single -
-     .replace(/^-+/, '') 		    // Trim - from start of text
-     .replace(/-+$/, '') 		    // Trim - from end of text
- }
+  let accentlessText = lowercasedstring
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
 
- module.exports = { tagsSorted, slugify }
+  return accentlessText
+    .replace(/\s+/g, "-") // Replace spaces with -
+    .replace(/[^\w-]+/g, "") // Remove all non-word chars
+    .replace(/--+/g, "-") // Replace multiple - with single -
+    .replace(/^-+/, "") // Trim - from start of text
+    .replace(/-+$/, "") // Trim - from end of text
+    .replace(/:/g, "") // remove colon
+}
+
+module.exports = { tagsSorted, slugify }
