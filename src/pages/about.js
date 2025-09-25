b5424a5392ae288848d0a2aee27f5f5a154800e4
socraticDevBlog <maximebonin@outlook.com>
2025-09-24 20:20:02 -0400

    pgp key

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,6 +2,48 @@ import React from "react"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
+const pgpKey = `-----BEGIN PGP PUBLIC KEY BLOCK-----
+
+mQGNBGjUde8BDACYyIHAq00/MekRSLtTYpL81yHZXmH+SkQIJZw6ld7ERBLWOPew
+YU49pcvDcFCh6DofuVERNeYE2Eys8h26M94U7sBn4vKr7ujZ3iVtn8P6krOzf22i
+n8Wzs9E+UzedBZcD0aWuf/Y10C13jVuG6ZfX2za5eLbqPp3Y584ufgUDLcugjNv1
+1a5fZRegC3NsZdcZbuykUT1uCoLTmhVWZl4U1yRelkXGX6/sVdOjozp7pFqcrPVL
+al2PYi60vtmP44iSylxCl82D6wMvF66kg8MT8OsLfc8/h7V7SBGoYQHJMDOdOD1X
+4AomZH4Vg6PXxRGGkIkvXJdJLpla/lFSlSPOtzdc0YKLZSAlsDR+djOi1iqZTlJe
+jMvHt4MjUN/CPUg6sLMnZ3eWVbtP0AyQnpryvJdv+UrEG8pdhlysQh89GKWNAKX0
+b3r8ZaY7kGZ6rtT/gXgi7SgcfHoT2nYmFRiqS9edsAWPPpC3zVX18oLs9vtPDuj8
++g23ZXehrnQiC8MAEQEAAbQgc29jcmF0aWNEZXYgPGhlbGxvQHNvY3JhdGljLmRl
+dj6JAdQEEwEKAD4WIQTKxWMjhkwQ8uBD4f3Sm1GVc6rd2AUCaNR17wIbAwUJAeEz
+gAULCQgHAgYVCgkICwIEFgIDAQIeAQIXgAAKCRDSm1GVc6rd2DIKC/4qkmW8whtw
+Nkgs+y1TZaJjWDgxrCnCrKunVzT9eW9axpQN+pO5sBPKpAJyi6LF0SBkDwyyN4aL
+WAB5OJrX+1vdBnE4E6qCe4KhtM/88k9Mq3Flj+UzFgPrqr0Tp58RTplY9bFzMysI
+4sDRycCoxKkxFC3hjq4o6VPv/fgnkcbWXI41qOKhb3TXYff5o2PRH+WAbyeFcRoD
+38h+VzBPe+LW+KqiXhUS80gKtW5X8eY7UFFh3HthpMnMnCEpnEX1M1HYBNt0ZWoD
+0LDA0OR2caPh7Ta3O6mfvgYQ3Z0wrxk3/iEuKHjYk0OKB3cWSt4Ei4DFKChwBv34
+kTXvmnyjY+yrwJD6tjpG5ovEyhwWIaOMjXV5CtJxVk6iJjZFAW6kGDqYmdKvY3ZX
+ZCcTeLq3iDo4jx+mpb2a2IZ4KRyEGslBlOXS7FEQXcQkPL/oWI1ncB03vDyeqPFC
+g9zdP4uBNIHjorRCyz06iACRqRukTBgfyVf6XFEyaMe4sEDiLbwiSIa5AY0EaNR1
+7wEMAPFZEZc5xCz1XBnBnVfRfOVj7m+w9q2MDJOHNU14JvQ+hbHTvNNoBCneg1Ws
+s3rYeOuAAg018iD87G9tb9p65FRsCvI7tjkMgJb4s6Xt7eE0BU/GtNMU6k+2GgMt
+WIL8ZAYD4+7sCaPHQEUfu9tTkz71gevanXSkCQ4UZ02lssDEs9F2ZMitL2vWyxcn
+E+/wul3XbcxURTgPxlAuIvsM00ehT9jCZdYs32mh+lK9ozHmStQFX0SSRM+3h4td
+NrrgVxjg0YKbpTETRsbSFyUsnhh/MppMqbrEr8wMNPJfD4ykaL5PTIjNgeofFvzH
+8qNxp+6CFYfKGcOucq/tiIqJgZnpTToucyIRR3gmXCb64Mip7JyG+8rNuPwnZyK+
+Yg2RDgOvaasb6fa6kvjIl+Xis8Fkvdt46KrQACMdsdw5L87R5j3q14SYQmHuLERC
+qrPV3avENns1VK/eIZG3nS5zQyxpeosNM6YZ6eKL28cjbKMdBoBl4H5dKAEpRu0N
+Qu/8LQARAQABiQG8BBgBCgAmFiEEysVjI4ZMEPLgQ+H90ptRlXOq3dgFAmjUde8C
+GwwFCQHhM4AACgkQ0ptRlXOq3dgw9gv/dIxkEol4u0dRD7o4dyfQjaTRqpjdpcNV
+tbikhcmxt1YcD/QC0qlWGTErytAZH+PE3lLlEhqd1wvAn7nZMZzZLOhME6buuTOC
+oRAKoTdrK5MlwpzSBJx37a9ntwBxQ9xadCdOXCB+CsljxbS9Jh+37N9+dIXx0zIo
+fWbHnadR6WFN/mB+7LGmnzw9b+IIdef4k986eYOWRQ3YeJ9SX5MjB9F7xiq8wgCz
+5XHn8DPdOfUFb2m0ov/+i5fTRS/dVSSpMYQQpEDb9pvvgnapC5/IymYdaFC6Y3Ks
+uDCioNXbVi8ITsZKmWW2puVERVBjp55+hqhWq8cqu+DMQftYRo5j4Af/t9YGm6J2
+nZAo5vvDBTohKR1upsOQdryhuYIlRzhKnSiuut6bnw9bNqwFQvzrC5A0lw7uDVAA
+QSGFI//xFuyKJCuti5Fc/moJcIS/SR2j7F88vuDnRxyouPjMkYbZW518yGo39dMd
+ehrRuRyMP5bqGbMpC3bzCKqtS1jqLlPj
+=D+7h
+-----END PGP PUBLIC KEY BLOCK-----`;
+
 const AboutPage = () => (
   <Layout pageTitle="Qu'est-ce que le blogue de SocraticDev ?">
     <Seo
@@ -34,6 +76,21 @@ const AboutPage = () => (
       d'écrire à propos de n'importe quoi. La plupart des articles portent ou
       bien sur la technologie ou bien sur la philosophie.
     </p>
+
+    <section id="contact">
+<h3>PGP signature</h3>
+<p>socraticDev ne contacte pas les gens qu'il ne connaît pas</p>
+
+<p>Utilisez PGP pour valider la signature des messages transmis par email ou média sociaux</p>
+
+
+  <p><strong>PGP Fingerprint:</strong>
+    CAC5 6323 864C 10F2 E043  E1FD D29B 5195 73AA DDD8
+  </p>
+
+<pre>{pgpKey}</pre>
+
+</section>
   </Layout>
 )
 
